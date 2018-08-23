b6d08477661370b18a74eba20e37ca4f8776e2aa
arslanah99 <Arah09@hotmail.com>
2018-08-23 22:04:56 +0000

    Adds looping exercises

---
+++ chorus-while.js
@@ -0,0 +1,10 @@
+var chours = "Lets dance!";
+var repeat = 0;
+while(repeat < 10) {
+  if (repeat === 5) {
+    console.log("Change Key");
+  }
+  console.log(chours);
+  repeat++
+}
+console.log("Until the sun comes up!");
